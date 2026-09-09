51d6a2d07cfecbfcfb8f7370c2cc0411567fbd1d
kruthika <harshithashanthraju@gmail.com>
2026-09-09 22:32:13 +0530

    updated jpa task with jpql queries & annotation

--- GPay/src/main/java/com/xworkz/bank/gpay/dto/PaymentDTO.java
+++ GPay/src/main/java/com/xworkz/bank/gpay/dto/PaymentDTO.java
@@ -1,5 +1,7 @@
 package com.xworkz.bank.gpay.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -10,7 +12,11 @@ import lombok.*;
 @ToString
 public class PaymentDTO {
 
+    @NotNull(message="id cannot be null")
     private Integer id;
+    @NotNull(message = "name canot be null")
     private String senderName;
+    @NotNull(message = "amount cannot be null")
+    @NotBlank(message = "amount id required")
     private Double amount;
 }
--- GPay/src/main/java/com/xworkz/bank/gpay/runner/PaymentRunner.java
+++ GPay/src/main/java/com/xworkz/bank/gpay/runner/PaymentRunner.java
@@ -16,53 +16,53 @@ public class PaymentRunner {
         PaymentService service = new PaymentServiceImpl();
 
 
-        //boolean isSaved = service.save(paymentDTO);
-        //System.out.println(isSaved);
+        boolean isSaved = service.save(paymentDTO);
+        System.out.println(isSaved);
 
 
-        List<PaymentDTO> paymentDTOList = new ArrayList<PaymentDTO>();
-
-        paymentDTOList.add(new PaymentDTO(5,"Harshitha", 500.0));
-
-        paymentDTOList.add(new PaymentDTO(6,"Anu", 1000.0));
-
-        paymentDTOList.add(new PaymentDTO(7,"Kiran", 2500.0));
-
-        paymentDTOList.add(new PaymentDTO(8,"Harsha", 1500.0));
-
-        paymentDTOList.add(new PaymentDTO(9,"Ravi", 3000.0));
-
-
-        //Boolean saved = service.saveAll(paymentDTOList);
-        //System.out.println(saved);
-
-
-        PaymentDTO getByIds = service.getById(1);
-        System.out.println(getByIds);
-
-        List<PaymentDTO> readAll = service.readAllPaymentDto();
-        System.out.println(readAll);
-
-        PaymentDTO getBySenderName = service.getPaymentBySenderName("Harshitha");
-        System.out.println(getBySenderName);
-
-        PaymentDTO getByAmount = service.getPaymentByAmount(500.0);
-        System.out.println(getByAmount);
-
-        List<PaymentDTO> getBySenderNameAndAmount = service.getPaymentBySenderNameAndAmount("Harshitha", 500.0);
-        System.out.println(getBySenderNameAndAmount);
-
-        List<PaymentDTO> getBySenderNameOrAmount = service.getPaymentBySenderNameOrAmount("Harshitha", 500.0);
-        System.out.println(getBySenderNameOrAmount);
-
-        List<PaymentDTO> getByAmountGreaterThan = service.getPaymentByAmountGreaterThan(1000.0);
-        System.out.println(getByAmountGreaterThan);
-
-        String updateSenderName=service.updateSenderNameById("riya",1);
-        System.out.println(updateSenderName);
-
-        String updateAmount=service.updateAmountById(1000.0,2);
-        System.out.println(updateAmount);
+//        List<PaymentDTO> paymentDTOList = new ArrayList<PaymentDTO>();
+//
+//        paymentDTOList.add(new PaymentDTO(5,"Harshitha", 500.0));
+//
+//        paymentDTOList.add(new PaymentDTO(6,"Anu", 1000.0));
+//
+//        paymentDTOList.add(new PaymentDTO(7,"Kiran", 2500.0));
+//
+//        paymentDTOList.add(new PaymentDTO(8,"Harsha", 1500.0));
+//
+//        paymentDTOList.add(new PaymentDTO(9,"Ravi", 3000.0));
+//
+//
+//        //Boolean saved = service.saveAll(paymentDTOList);
+//        //System.out.println(saved);
+//
+//
+//        PaymentDTO getByIds = service.getById(1);
+//        System.out.println(getByIds);
+//
+//        List<PaymentDTO> readAll = service.readAllPaymentDto();
+//        System.out.println(readAll);
+//
+//        PaymentDTO getBySenderName = service.getPaymentBySenderName("Harshitha");
+//        System.out.println(getBySenderName);
+//
+//        PaymentDTO getByAmount = service.getPaymentByAmount(500.0);
+//        System.out.println(getByAmount);
+//
+//        List<PaymentDTO> getBySenderNameAndAmount = service.getPaymentBySenderNameAndAmount("Harshitha", 500.0);
+//        System.out.println(getBySenderNameAndAmount);
+//
+//        List<PaymentDTO> getBySenderNameOrAmount = service.getPaymentBySenderNameOrAmount("Harshitha", 500.0);
+//        System.out.println(getBySenderNameOrAmount);
+//
+//        List<PaymentDTO> getByAmountGreaterThan = service.getPaymentByAmountGreaterThan(1000.0);
+//        System.out.println(getByAmountGreaterThan);
+//
+//        String updateSenderName=service.updateSenderNameById("riya",1);
+//        System.out.println(updateSenderName);
+//
+//        String updateAmount=service.updateAmountById(1000.0,2);
+//        System.out.println(updateAmount);
 
     }
 }
